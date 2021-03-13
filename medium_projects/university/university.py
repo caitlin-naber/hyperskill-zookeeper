b09a95aa7d6c926f33728900c8a884ff6bfa853f
caitlin-naber <56165420+caitlin-naber@users.noreply.github.com>
2021-03-13 10:10:09 -0500

    Create university.py
    Stage 1 of University Admissions project

---
+++ medium_projects/university/university.py
@@ -0,0 +1,9 @@
+import statistics
+
+test_1 = float(input())
+test_2 = float(input())
+test_3 = float(input())
+
+mean = statistics.mean([test_1, test_2, test_3])
+print(mean)
+print('Congratulations, you are accepted!')

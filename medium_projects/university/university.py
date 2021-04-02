20d6c079b1369bd04e28be984da64b41f3abb127
caitlin-naber <56165420+caitlin-naber@users.noreply.github.com>
2021-04-02 10:17:54 -0400

    Update university.py
    update to stage 3

--- medium_projects/university/university.py
+++ medium_projects/university/university.py
@@ -1,12 +1,28 @@
-import statistics
+num_apps = int(input())
+num_accepts = int(input())
+students = []
 
-test_1 = float(input())
-test_2 = float(input())
-test_3 = float(input())
+def accepted_students(students):
+    counter = 0
+    while counter < num_accepts:
+        print(students[counter][0])
+        counter += 1
 
-mean = statistics.mean([test_1, test_2, test_3])
-print(mean)
-if mean >= 60:
-    print('Congratulations, you are accepted!')
-else:
-    print('We regret to inform you that we will not be able to offer you admission.')
+
+for n in range(num_apps):
+    student = input()
+    student = list(student.split())
+    first_name = student[0]
+    last_name = student[1]
+    gpa = float(student[2])
+    stu_name = first_name + ' ' + last_name
+    student = [stu_name, gpa]
+    students.append(student)
+    sorted_students = sorted(students)
+
+students = sorted(students, key=lambda students: (-students[1], students[0]))
+
+print('Successful applicants:')
+accepted_students(students)
+
+# TODO: resolve ties, same GPAs should be sorted alpha order, use lambda for key
